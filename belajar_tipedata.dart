8a90c926f07945acb1690686d25868a2f95fd6fe
Vian-All <alvian01sanjaya@gmail.com>
2026-08-31 19:47:52 +0700

    versi 3

--- belajar_tipedata.dart
+++ belajar_tipedata.dart
@@ -1,10 +1,44 @@
 void main() {
-  String nama = 'Deni';
-  int umur = 100;  
-  bool sudahmenikah = true;
+  //String nama = 'Deni';
+  //int umur = 100;  
+  //bool sudahmenikah = true;
 
-  var ramai = 'Deni';
-  var angka1 = 100; 
+  //var ramai = 'Deni';
+  //var angka1 = 100; 
   
-  print('halo nama saya $nama, umur saya $umur, sudah menikah? $sudahmenikah');
+  //print('halo nama saya $nama, umur saya $umur, sudah menikah? $sudahmenikah');
+
+  //double angka2 = 20.5;
+
+  //num hasil = 0,5;
+  //hasil = angka1 + angka2;
+  //print(hasil); 
+
+  //dynamic angka1 = 100;
+  //dynamic angka2 = 200.5;
+  //dynamic hasil = 0;
+  //hasil = angka1 + angka2;
+  //print(hasil);
+
+  //var angka1 = 100;
+  //var angka2 = 200.5;
+  //var hasil = 0.5;
+  //hasil = angka1 + angka2;
+  //print(hasil);
+
+  //list (array) => kumpulan data yang memiliki index
+  //List<dynamic> namamhs = ['deni', 'sindy', 'niki', 100, true];
+  //List<String> nama = ['deni', 'sindy', 'niki'];
+  //List<num> angka = [12, 12.5, 13.0, -1, -11.5];
+  //print(namamhs[2]); >= mencari 
+
+  //List namamhs = [];
+  //namamhs.add('Wilson1');
+  //namamhs.add('Wilson2');
+  //namamhs.add('Wilson3');
+  //namamhs.add('Wilson4');
+  //print(namamhs.reversed);
+  //print(namamhs.add);
+  //print(namamhs.remove(3));
+  //print(namamhs.indexOf(Wilson3));
 }
