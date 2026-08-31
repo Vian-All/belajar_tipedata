17d2e553609d29ec41e6717d9a57f44f4749c54a
Vian-All <alvian01sanjaya@gmail.com>
2026-08-31 19:19:51 +0700

    versi 1

---
+++ belajar_tipedata.dart
@@ -0,0 +1,10 @@
+void main() {
+  String nama = 'Deni';
+  int umur = 100;  
+  bool sudahmenikah = false;
+
+  var ramai = 'Deni';
+  var angka1 = 100; 
+  
+  print('halo nama saya $nama, umur saya $umur, sudah menikah? $sudahmenikah');
+}
